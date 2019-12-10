de208c953720e19205a0617fa81c718a6f489467
Nandan <nandanmarkrao@gmail.com>
2019-12-10 23:35:53 +0100

    Added simulate notebook, basic trees working

--- test_trees.py
+++ test_trees.py
@@ -4,7 +4,21 @@ import trees as t
 def test_mse():
     assert t.mse(None, np.array([10.,10.,10.,10.,30.,30.,30.,30.]))[1] == 100.
     assert t.mse(None, np.array([10.,10.,10.,10.]))[1] == 0.
-    assert t.mse(None, np.array([5,5,5,5,10,10,10,10]))[1] == 6.25
+    assert t.mse(None, np.array([5.,5.,5.,5.,10.,10.,10.,10.]))[1] == 6.25
+
+def test_get_indices():
+    ar = np.array([0,0,0,1,2,3,3,3,5]).reshape(-1, 1)
+    idxs = t.get_indices(ar, 0, 2, 7)
+    assert np.all(idxs == np.array([3,4,5]))
+    idxs = t.get_indices(ar, 0, 1, 8)
+    assert np.all(idxs == np.array([3,4,5,8]))
+    idxs = t.get_indices(ar, 0, 0, 9)
+    assert np.all(idxs == np.array([0,3,4,5,8]))
+
+def test_get_indices_works_with_one_el():
+    ar = np.array([0,0,0,1,2,3,3,3,5], dtype=np.float64).reshape(-1, 1)
+    idxs = t.get_indices(ar, 0, 3, 3)
+    assert np.all(idxs == np.array([3]))
 
 def test_find_threshold_mse():
     # x,y
@@ -12,10 +26,11 @@ def test_find_threshold_mse():
     # should go through x and get score at every split
 
     X = np.array([1,2,3,4,5,6,7,8]).reshape(-1, 1)
-    y = np.array([5,5,5,5,10,10,10,10])
-    idx,score = t.find_threshold(t.mse, X, y, 1, X.shape[0])
+    y = np.array([5,5,5,5,10,10,10,10], dtype=np.float64)
+    idx,score,thresh = t.find_threshold(t.mse, X, y, 0, 1, X.shape[0] - 1)
     assert idx == 4
     assert score == 2.5**2
+    assert thresh == 4.5
 
 def test_find_threshold_mae():
     # x,y
@@ -23,12 +38,13 @@ def test_find_threshold_mae():
     # should go through x and get score at every split
 
     X = np.array([1,2,3,4,5,6]).reshape(-1, 1)
-    y = np.array([5,10,15,25,30,35])
-    idx,score = t.find_threshold(t.mae, X, y, 1, X.shape[0])
+    y = np.array([5,10,15,25,30,35], dtype=np.float64)
+    idx,score,thresh = t.find_threshold(t.mae, X, y, 0, 1, X.shape[0] - 1)
     assert idx == 3
     assert score == 10 - np.mean([5., 0., 5.])
+    assert thresh == 3.5
 
-def test_sort_and_extract():
+def test_sort_for_dim():
     X = np.array([[1,10],
                   [2,9],
                   [3,8],
@@ -37,7 +53,7 @@ def test_sort_and_extract():
                   [6,5],
                   [7,4],
                   [8,3]])
-    y = np.array([10,20,30,40,50,60,70,80])
+    y = np.array([10,20,30,40,50,60,70,80], dtype=np.float64)
 
     # sorts by given x and returns 2-d array
     xo,yo = t.sort_for_dim(X, y, 0)
@@ -66,9 +82,9 @@ def test_find_next_split_1():
                   [6,5],
                   [7,4],
                   [8,3]])
-    y = np.array([10,20,30,40,50,60,70,80])
+    y = np.array([10,20,30,40,50,60,70,80], dtype=np.float64)
     nxt = t.find_next_split(t.mse, X, y, np.array([0,1]), 2)
-    assert nxt == t.Split(0, 4, 5, 400.)
+    assert nxt == t.Split(0, 4, 4.5, 400.)
 
 def test_find_next_split_2():
     X = np.array([[1,10],
@@ -79,9 +95,9 @@ def test_find_next_split_2():
                   [60,5],
                   [7,4],
                   [8,3]])
-    y = np.array([10,20,30,40,50,60,70,80])
+    y = np.array([10,20,30,40,50,60,70,80], dtype=np.float64)
     nxt = t.find_next_split(t.mse, X, y, np.array([0,1]), 2)
-    assert nxt == t.Split(1, 4, 7, 400.)
+    assert nxt == t.Split(1, 4, 6.5, 400.)
 
 def test_split_data():
     X = np.array([[1,10],
@@ -92,7 +108,7 @@ def test_split_data():
                   [60,5],
                   [7,4],
                   [8,3]])
-    y = np.array([10,20,30,40,50,60,70,80])
+    y = np.array([10,20,30,40,50,60,70,80], dtype=np.float64)
     split = t.Split(1,3,6,100.)
     Xl, yl, Xr, yr = t.split_data(X, y, split)
     assert np.all(Xl == np.array([[8,3],
@@ -119,7 +135,7 @@ def test_build_tree_obeys_min_samples():
                   [60,5],
                   [7,4],
                   [8,3]])
-    y = np.array([10,20,30,40,50,60,70,80])
+    y = np.array([10,20,30,40,50,60,70,80], dtype=np.float64)
 
     tree = t.build_tree(t.mse, X, y,
                         dims = np.array([0,1]),
@@ -127,9 +143,9 @@ def test_build_tree_obeys_min_samples():
                         min_gain = 0.1,
                         min_samples = 2)
 
-    assert tree.thresh == 7.0
-    assert tree.left.thresh == 5.0
-    assert tree.right.thresh == 9.0
+    assert tree.thresh == 6.5
+    assert tree.left.thresh == 4.5
+    assert tree.right.thresh == 8.5
     assert type(tree.left.left) == t.Leaf
     assert type(tree.right.right) == t.Leaf
 
@@ -139,6 +155,45 @@ def test_build_tree_obeys_min_samples():
                         min_gain = 0.1,
                         min_samples = 3)
 
-    assert tree.thresh == 7.0
+    assert tree.thresh == 6.5
     assert type(tree.left) == t.Leaf
     assert type(tree.right) == t.Leaf
+
+    tree = t.build_tree(t.mse, X, y,
+                        dims = np.array([0,1]),
+                        k = 30,
+                        min_gain = 0.1,
+                        min_samples = 1)
+
+    assert tree.thresh == 6.5
+    assert tree.left.thresh == 4.5
+
+    # TODO: the flip of dims isn't actually desired behavior
+    # it's just a side effect of the order of argmax returning
+    # first on a tie
+    assert tree.left.left.dim == 0
+    assert tree.left.left.thresh == 7.5
+    assert tree.right.thresh == 8.5
+    assert tree.right.right.dim == 0
+    assert tree.right.right.thresh == 10.5
+
+
+# def test_build_tree_works_with_repetitions():
+#     X = np.array([[0,10],
+#                   [0,9],
+#                   [0,8],
+#                   [0,7],
+#                   [0,6],
+#                   [2,5],
+#                   [3,4],
+#                   [10,3]])
+#     y = np.array([10,20,30,40,50,60,70,80])
+
+#     tree = t.build_tree(t.mse, X, y,
+#                         dims = np.array([0,1]),
+#                         k = 30,
+#                         min_gain = 0.1,
+#                         min_samples = 2)
+
+#     print(tree)
+#     assert False
--- trees.py
+++ trees.py
@@ -2,8 +2,14 @@ import numpy as np
 from numba import njit
 from collections import namedtuple
 
+# TODO:
+#
+# 1. Add support for weighting
+# 2. Create Criterion for treatment effects
+
 @njit
 def mse(X, y):
+    # add weights as idx in X and use in Criterion???
     m = np.mean(y)
     a = (m - y)
     mse = a.dot(a) / a.shape[0]
@@ -16,21 +22,46 @@ def mae(X, y):
     return med, mae
 
 @njit
-def find_threshold(crit, X, y, mn, mx):
-    N = X.shape[0]
-    a = np.zeros(mx - mn)
+def np_unique(a):
+    prev = a[0]
+    unique = [a[0]]
+    idxs = [0]
+    for i,x in enumerate(a):
+        if x != prev:
+            unique.append(x)
+            idxs.append(i)
+        prev = x
+    return np.array(unique), np.array(idxs)
 
-    # bucket for large x? No need to enumerate everything,
+@njit
+def get_indices(X, p, mn, mx):
+    _, idxs = np_unique(X[:, p])
+    return idxs[(idxs >= mn) & (idxs <= mx)]
+
+
+@njit()
+def find_threshold(crit, X, y, p, mn, mx):
+    N = X.shape[0]
+
+    # bucket for large x? No need to enumerate everything!
     _, base = crit(X,y)
-    for j,i in enumerate(range(mn, mx)):
-        # should this always be the mean?
+    idxs = get_indices(X, p, mn, mx)
+    loss, idx, thresh = np.inf, idxs[0], X[idxs[0], p]
+
+    for i in idxs:
         _, left = crit(X[:i,:], y[:i])
         _, right = crit(X[i:,:], y[i:])
-        a[j] = (left + right) / 2
 
-    gain = base - a
-    gidx = np.argmax(gain)
-    return gidx + mn, gain[gidx]
+        # should this always be the mean?
+        left_share = X[:i].shape[0] / N
+        right_share = 1 - left_share
+        curr_loss = left*left_share + right*right_share
+
+        if curr_loss < loss:
+            loss, idx = curr_loss, i
+            thresh = (X[i, p] + X[i-1, p])/2
+
+    return idx, base - loss, thresh
 
 @njit
 def sort_for_dim(X, y, p):
@@ -39,9 +70,10 @@ def sort_for_dim(X, y, p):
 
 Split = namedtuple('Split', ['dim', 'idx', 'thresh', 'gain'])
 Node = namedtuple('Node', ['dim', 'thresh', 'gain', 'left', 'right'])
-Leaf = namedtuple('Leaf', ['prediction', 'score'])
+Leaf = namedtuple('Leaf', ['prediction', 'score', 'N'])
 
-@njit
+
+@njit(parallel=True)
 def find_next_split(crit, X, y, dims, min_samples):
     # pick dim with greatest gain (from dims)
     # return dim/threshold/gain
@@ -51,18 +83,16 @@ def find_next_split(crit, X, y, dims, min_samples):
     gains, thresholds = np.zeros(P), np.zeros(P)
 
     # If there are not enough elements to split, stop
-    mn, mx = min_samples, N - min_samples + 1
-    if mn >= mx:
+    mn, mx = min_samples, N - min_samples
+    if mn > mx:
         return None
 
     for p in dims:
         xi,yi = sort_for_dim(X, y, p)
-        indices[p], gains[p] = find_threshold(crit, xi, yi, mn, mx)
-        thresholds[p] = xi[indices[p], p]
+        i,g,t = find_threshold(crit, xi, yi, p, mn, mx)
+        indices[p], gains[p], thresholds[p] = i,g,t
 
     dim = np.argmax(gains)
-    idx_max = indices[dim]
-
     return Split(dim, indices[dim], thresholds[dim], gains[dim])
 
 @njit
@@ -73,28 +103,35 @@ def split_data(X, y, split):
     return Xo[:idx, :], yo[:idx], Xo[idx:, :], yo[idx:]
 
 
-def pick_split(x, node):
-    if x[node.dim] >= node.thresh:
-        return node.right
-    return node.left
-
-
 def build_tree(crit, X, y, dims, k, min_gain, min_samples):
     if k == 0:
-        return Leaf(*crit(X, y))
+        return Leaf(*crit(X, y), y.shape[0])
 
     split = find_next_split(crit, X, y, dims, min_samples)
 
     if split is None:
-        return Leaf(*crit(X, y))
+        return Leaf(*crit(X, y), y.shape[0])
 
     Xl, yl, Xr, yr = split_data(X, y, split)
     dim, thresh, gain = split.dim, split.thresh, split.gain
 
     # stop if one side is empty
     if yl.shape[0] == 0 or yr.shape[0] == 0 or gain < min_gain:
-        return Leaf(*crit(X, y))
+        return Leaf(*crit(X, y), y.shape[0])
 
     return Node(dim, thresh, gain,
                 left = build_tree(crit, Xl, yl, dims, k-1, min_gain, min_samples),
                 right = build_tree(crit, Xr, yr, dims, k-1, min_gain, min_samples))
+
+
+def pick_split(x, node):
+    if x[node.dim] >= node.thresh:
+        return node.right
+    return node.left
+
+def predict(x, node):
+    while True:
+        try:
+            node = pick_split(x, node)
+        except AttributeError:
+            return node.prediction
