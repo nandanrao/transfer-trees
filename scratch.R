4d9f74c1da39e80b493f1f91033ab39de79208e2
Nandan <nandanmarkrao@gmail.com>
2020-01-15 17:15:24 +0100

    simulation of honest and conf intervals

--- scratch.R
+++ scratch.R
@@ -20,25 +20,48 @@ mse <- mean((predict(tree) - (simulation.1$x1 / 2))**2)
 
 sim_dat <- read.csv('sim_dat-2.csv')
 
+## idx <- sample(nrow(sim_dat))
+## sim_dat <- sim_dat[idx, ]
 
 N = nrow(sim_dat)
-cutoff = round(N/2)
+cutoff = 1000
 
 train <- sim_dat[0:cutoff, ]
-test <- sim_dat[cutoff:N, ]
+test <- sim_dat[(cutoff+1):N, ]
+## w = rep(1/cutoff, cutoff)
 
 tree <- causalTree(y ~ X1 + X2 + X3 + X4 + X5 + X6,
                    data = train,
                    treatment = sim_dat$treatment,
                    split.Rule = "CT",
                    cv.option = "CT",
-                   split.Honest = T,
+                   split.Honest = F,
                    cv.Honest = F,
-                   split.Bucket = F,
-                   xval = 10,
-                   split.alpha = 0.1,
+                   ## HonestSampleSize = 500,
+                   split.Bucket = FALSE,
+                   xval = 3,
                    minsize = 25)
 
+opcp <- tree$cptable[,1][which.min(tree$cptable[,4])]
+t <-prune(tree, opcp)
 
+1 - mean((predict(t, newdata = test) - test$tau)**2) / var(test$tau)
 
-1 - mean((predict(tree, newdata = test) - test$tau)**2) / var(sim_dat$tau)
+tree <- honest.causalTree(y ~ X1 + X2 + X3 + X4 + X5 + X6,
+                   data = train[0:500, ],
+                   est_data = train[501:cutoff, ],
+                   treatment = sim_dat[0:500, ]$treatment,
+                   est_treatment = sim_dat[501:cutoff, ]$treatment,
+                   split.Rule = "CT",
+                   cv.option = "CT",
+                   split.Honest = T,
+                   cv.Honest = T,
+                   ## HonestSampleSize = 500,
+                   split.Bucket = FALSE,
+                   xval = 3,
+                   minsize = 25)
+
+opcp <- tree$cptable[,1][which.min(tree$cptable[,4])]
+t <-prune(tree, opcp)
+t
+tree
